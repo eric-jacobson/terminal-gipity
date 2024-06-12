086860cfe635c3ee8f66dfb4b6d354b5e265c947
eric-jacobson <ecjacobson9@gmail.com>
2024-06-11 21:26:41 -0600

    basic application loop running that simply echoes the input

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,55 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"strings"
 
+	"github.com/nsf/termbox-go"
+)
+
+func main() {
+	err := termbox.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer termbox.Close()
+
+	const SPACE = " "
+	const BACKSPACE = "\x08"
+
+	var input strings.Builder
+
+	fmt.Print(">")
+
+main_loop:
+	for {
+		switch keyPress := termbox.PollEvent(); keyPress.Type {
+		case termbox.EventKey:
+			switch keyPress.Key {
+			case termbox.KeyEsc:
+				break main_loop
+			case termbox.KeyEnter:
+				echo(input.String())
+				input.Reset()
+				break
+			case termbox.KeyBackspace, termbox.KeyBackspace2:
+				input.WriteString(BACKSPACE)
+				fmt.Print(BACKSPACE)
+			case termbox.KeySpace:
+				input.WriteString(SPACE)
+				fmt.Print(SPACE)
+			default:
+				if keyPress.Ch != 0 {
+					char := fmt.Sprintf("%c", keyPress.Ch)
+					input.WriteString(char)
+					fmt.Print(char)
+				}
+			}
+		}
+	}
+}
+
+func echo(input string) {
+	fmt.Printf("\n\tEcho: %v\n>", input)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/eric-jacobson/terminal-gipity
 
 go 1.22.3
+
+require (
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
+)
